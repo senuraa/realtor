80066cf72c6d3697a87921a110dd8d086682b26c
Senura Seneviratne <senuraa@msn.com>
2018-06-12 15:31:57 +0530

    appointment per ad limit added

--- server/controllers/appointments.js
+++ server/controllers/appointments.js
@@ -22,18 +22,25 @@ exports.addAppointments = function (req, res) {
     var app_date = req.body.datetime;
     var comments = req.body.comments;
     console.log(req.body);
-    var newApp = new Apps({ phone_number: phone_number, ad_id: mongoose.Types.ObjectId(ad_id), app_date: app_date, comments: comments, status: 0,adIdStr:ad_id })
-    newApp.save(function (err, apps) {
-        if (err) {
-            throw err;
-            res.status(500).json({ "message": "Error creating the appointment" })
+    Apps.find({ phone_number: phone_number, ad_id: mongoose.Types.ObjectId(ad_id) }).exec(function (err, docs) {
+        if (docs.length > 0) {
+            res.status(500).json({ "message": "Appointment exists" })
         } else {
-            console.log('Appointment created');
-            res.status(200).json(apps)
+            var newApp = new Apps({ phone_number: phone_number, ad_id: mongoose.Types.ObjectId(ad_id), app_date: app_date, comments: comments, status: 0, adIdStr: ad_id })
+            newApp.save(function (err, apps) {
+                if (err) {
+                    throw err;
+                    res.status(500).json({ "message": "Error creating the appointment" })
+                } else {
+                    console.log('Appointment created');
+                    res.status(200).json(apps)
+                }
+
+
+            })
         }
-
-
     })
+
 }
 
 exports.getAppointments = function (req, res) {
@@ -42,26 +49,26 @@ exports.getAppointments = function (req, res) {
     var app;
     var ad;
 
-    Apps.find({phone_number:phone_number,status:{$lt:status}}).populate('ad_id').exec(function(err,docs){
-        if(err){
+    Apps.find({ phone_number: phone_number, status: { $lt: status } }).populate('ad_id').exec(function (err, docs) {
+        if (err) {
             console.log(err);
-            res.status(500).json({"error":"error getting appointments"})
-        }else{
+            res.status(500).json({ "error": "error getting appointments" })
+        } else {
             res.status(200).json(docs);
         }
     })
 }
-exports.changeAppointmentStatus = function(req,res){
-    var id=req.body._id;
+exports.changeAppointmentStatus = function (req, res) {
+    var id = req.body._id;
     var status = req.body.status;
-    Apps.findByIdAndUpdate(id,{$set :{status:status}}).exec(function(err,docs){
-        if(err){
-            res.status(500).json({"message":"error changing status"})
-        }else{
-            res.status(200).json({"message":"status updated successfully"})
+    Apps.findByIdAndUpdate(id, { $set: { status: status } }).exec(function (err, docs) {
+        if (err) {
+            res.status(500).json({ "message": "error changing status" })
+        } else {
+            res.status(200).json({ "message": "status updated successfully" })
         }
     })
 }
-exports.getTopPicks=function(req,res){
-    
+exports.getTopPicks = function (req, res) {
+
 }
